845fdb4e53d8e2f2e1aeb2e7f38a4f18962d0ee3
Jeff <zhangzhuzhefu@gmail.com>
2018-04-17 20:25:11 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const {Wechaty, Room} = require('wechaty')
+const qr = require('qrcode-terminal')
+
+const bot = Wechaty.instance()
+
+bot
+.on('scan', (url, code)=>{
+    let loginUrl = url.replace('qrcode', 'l')
+    qr.generate(loginUrl)
+    console.log(url)
+})
+
+.on('login', user=>{
+    console.log(`${user} login`)
+})
+
+.on('friend', async function (contact, request){
+    if(request){
+        await request.accept()
+        console.log(`Contact: ${contact.name()} send request ${request.hello}`)
+    }
+})
+
+.on('message', async function(m){
+    const contact = m.from()
+    const content = m.content()
+    const room = m.room()
+
+    if(room){
+        console.log(`${contact.name()} @ ${room.topic()} : ${content}`)
+    } else{
+        console.log(`${contact.name()} : ${content}`)
+    }
+
+    if(m.self()){
+        return
+    }
+
+    if(/hello/.test(content)){
+        m.say("hello how are you")
+    }
+
+    if(/room/.test(content)){
+        let keyroom = await Room.find({topic: "test"})
+        if(keyroom){
+            await keyroom.add(contact)
+            await keyroom.say("welcome!", contact)
+        }
+    }
+
+    if(/out/.test(content)){
+        let keyroom = await Room.find({topic: "test"})
+        if(keyroom){
+            await keyroom.say("Remove from the room", contact)
+            await keyroom.del(contact)
+        }
+    }
+})
+
+.init()
+
