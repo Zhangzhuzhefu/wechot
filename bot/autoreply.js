a14e6f54decf34d433c07227bbaa0874df32f965
Jeff <zhangzhuzhefu@gmail.com>
2018-04-18 22:04:18 +0800

    - auto reply

--- bot/autoreply.js
+++ bot/autoreply.js
@@ -1,7 +1,7 @@
 
 const autoReply = {
-    JEFF_AT_WORK: '小蒜苗正在专心苦钱，他可能不能及时回复你的消息，有急事请给他打call',
-    JEFF_IS_SLEEPING: '小蒜苗已经去睡觉啦，他可能要早上醒来才能看到你的消息'
+    JEFF_AT_WORK: '[自动回复]小蒜苗正在专心苦钱并开启了消息免打扰，他可能不能及时回复你的消息，有急事请给他打call',
+    JEFF_IS_SLEEPING: '[自动回复]小蒜苗已经去睡觉啦，他可能要早上醒来才能看到你的消息'
 };
 
 module.exports = autoReply;
